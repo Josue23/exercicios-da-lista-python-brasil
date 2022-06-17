5e6395b1602bef7251ffaa53dacc4eb7b06a2b25
Josue23 <josuerodrigues@gmail.com>
2022-06-17 16:57:46 -0300

    Feito os exercícios 11, 12 e 13

--- estrutura_de_decisao.py
+++ estrutura_de_decisao.py
@@ -155,3 +155,116 @@ def cumprimentar(letra):
 
     else:
         print(f'Valor {letra}  inválido.')
+
+
+def aumento_de_salario(salario):
+    '''As Organizações Tabajara resolveram dar um aumento de salário aos seus colaboradores e lhe contraram para
+    desenvolver o programa que calculará os reajustes.
+    Faça um programa que recebe o salário de um colaborador e o reajuste segundo o seguinte critério, baseado no
+    salário atual:
+    salários até R$ 280,00 (incluindo) : aumento de 20%
+    salários entre R$ 280,00 e R$ 700,00 : aumento de 15%
+    salários entre R$ 700,00 e R$ 1500,00 : aumento de 10%
+    salários de R$ 1500,00 em diante : aumento de 5% Após o aumento ser realizado, informe na tela:
+    o salário antes do reajuste;
+    o percentual de aumento aplicado;
+    o valor do aumento;
+    o novo salário, após o aumento. '''
+    if salario <= 280:
+        percentual_de_aumento = 0.20
+        aumento = salario * percentual_de_aumento
+        salario_com_aumento = salario + aumento
+    elif 280 < salario < 700:
+        percentual_de_aumento = 0.15
+        aumento = salario * percentual_de_aumento
+        salario_com_aumento = salario + aumento
+    elif 700 < salario < 1500:
+        percentual_de_aumento = 0.10
+        aumento = salario * percentual_de_aumento
+        salario_com_aumento = salario + aumento
+    else:
+        percentual_de_aumento = 0.05
+        aumento = salario * percentual_de_aumento
+        salario_com_aumento = salario + aumento
+    print(f'''
+Salario: {salario}
+Percentual de aumento: {percentual_de_aumento * 100}
+Valor do aumento: {aumento:.2f}
+Salário reajustado: {salario_com_aumento:.2f}
+    ''')
+
+
+'''Como usar exercício 11:
+>>> aumento_de_salario(200)'''
+
+
+def folha_de_pagamentos(valor_hora, horas_trabalhadas_mes):
+    '''Faça um programa para o cálculo de uma folha de pagamento, sabendo que os descontos são do Imposto de Renda,
+    que depende do salário bruto (conforme tabela abaixo) e 3% para o Sindicato e que o FGTS corresponde a 11% do
+    Salário Bruto, mas não é descontado (é a empresa que deposita). O Salário Líquido corresponde ao Salário Bruto menos
+    os descontos. O programa deverá pedir ao usuário o valor da sua hora e a quantidade de horas trabalhadas no mês.
+
+    Desconto do IR:
+        Salário Bruto até 900 (inclusive) - isento
+        Salário Bruto até 1500 (inclusive) - desconto de 5%
+        Salário Bruto até 2500 (inclusive) - desconto de 10%
+        Salário Bruto acima de 2500 - desconto de 20% Imprima na tela as informações, dispostas conforme
+        o exemplo abaixo.
+    No exemplo o valor da hora é 5 e a quantidade de hora é 220.
+
+            Salário Bruto: (5 * 220)        : R$ 1100,00
+            (-) IR (5%)                     : R$   55,00
+            (-) INSS ( 10%)                 : R$  110,00
+            FGTS (11%)                      : R$  121,00
+            Total de descontos              : R$  165,00
+            Salário Liquido                 : R$  935,00'''
+
+    salario_bruto = valor_hora * horas_trabalhadas_mes
+    if salario_bruto <= 900:
+        imposto_renda = 0
+    elif salario_bruto <= 1500:
+        imposto_renda = salario_bruto * 0.05
+    elif salario_bruto <= 2500:
+        imposto_renda = salario_bruto * 0.10
+    else:
+        imposto_renda = salario_bruto * 0.20
+
+    inss = salario_bruto * 0.10
+    # sindicato = salario_bruto * 0.03
+    fgts = salario_bruto * 0.11  # não é descontado do salario bruto
+
+    descontos = imposto_renda + inss
+    salario_liquido = salario_bruto - descontos
+
+    print(f'''
+Salário Bruto: ({valor_hora} * {horas_trabalhadas_mes})        : R$ {salario_bruto:.2f}
+(-) IR (5%)                     : R$ {imposto_renda:.2f}
+(-) INSS ( 10%)                 : R$ {inss:.2f}
+FGTS (11%)                      : R$ {fgts:.2f}
+Total de descontos              : R$ {descontos:.2f}
+Salário Liquido                 : R$ {salario_liquido:.2f}''')
+
+
+def dia_da_semana(entrada):
+    '''Faça um Programa que leia um número e exiba o dia correspondente da semana. (1-Domingo, 2- Segunda, etc.),
+    se digitar outro valor deve aparecer valor inválido. '''
+
+    dias_da_semana = {
+        1: 'Domingo',
+        2: 'Segunda',
+        3: 'Terça',
+        4: 'Quarta',
+        5: 'Quinta',
+        6: 'Sexta',
+        7: 'Sábado',
+    }
+    if entrada in range(1, 8):
+        for chave, valor in dias_da_semana.items():
+            if entrada == chave:
+                return valor
+    else:
+        return f'Valor {entrada} inválido.'
+
+
+'''Como usar exercício 13:
+dia_da_semana(1)'''
